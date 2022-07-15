71ae7f541b2de54b42812e7bd2fd19dba2129057
Everton Santos <77086466+esantos92@users.noreply.github.com>
2022-07-14 21:57:34 -0300

    module 3_4 completed (#14)
    Co-authored-by: Everton Santos <everton.santos@redealumni.com>

--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -1,11 +1,20 @@
 <template>
   <Formulario @aoSalvarTarefa="salvarTarefa" />
   <div class="lista">
-    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
     <Box v-if="listaEstaVazia">
       Você não está muito muito produtivo hoje :(
     </Box>
 
+    <div class="field">
+      <p class="control has-icons-left has-icons-right">
+        <input class="input" type="text" placeholder="Digite para filtrar:" v-model="filtro">
+        <span class="icon is-small is-left">
+          <i class="fas fa-search"></i>
+        </span>
+      </p>
+    </div>
+
+    <Tarefa v-for="(tarefa, index) in tarefas" :key="index" :tarefa="tarefa" @aoTarefaClicada="selecionarTarefa" />
     <div class="modal" :class="{ 'is-active': tarefaSelecionada }" v-if="tarefaSelecionada">
       <div class="modal-background"></div>
       <div class="modal-card">
@@ -31,7 +40,7 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watchEffect } from 'vue';
 import Formulario from '../components/Formulario.vue';
 import Tarefa from '../components/Tarefa.vue';
 import Box from '../components/Box.vue';
@@ -55,7 +64,7 @@ export default defineComponent({
 
   computed: {
     listaEstaVazia(): boolean {
-      return this.tarefas.length === 0
+      return this.tarefas.length == 0
     }
   },
   methods: {
@@ -77,9 +86,18 @@ export default defineComponent({
     const store = useStore()
     store.dispatch(OBTER_TAREFAS)
     store.dispatch(OBTER_PROJETOS)
+
+    const filtro = ref('')
+
+    // const tarefas = computed(() => store.state.tarefa.tarefas.filter(t => !filtro.value || t.descricao.includes(filtro.value)))
+
+    watchEffect(() => {
+      store.dispatch(OBTER_TAREFAS, filtro.value)
+    })
     return {
-      tarefas: computed(() => store.state.tarefa.tarefas),
-      store
+      store,
+      filtro,
+      tarefas: computed(() => store.state.tarefa.tarefas)
     }
   }
 });
